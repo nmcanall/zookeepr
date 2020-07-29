405466a92ebd73f39e8c0db1b7958c9d31d795b0
nmcanall <nmcanall@citadel.edu>
2020-07-28 21:42:56 -0700

    find animal by ID

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const {animals} = require("./data/animals.json");
 
 // Instatiate the server
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Helper function to filter results based on query parameters
-const filterByQuery = function(query, animalsArray) {
+function filterByQuery(query, animalsArray) {
     let filteredResults = animalsArray;
 
     // Filter by name
@@ -43,6 +44,12 @@ const filterByQuery = function(query, animalsArray) {
     return filteredResults;
 }
 
+// Function to find a specific animal given its ID
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 // Add route to server
 app.get("/api/animals", (req, res) => {
     let results = animals;
@@ -52,8 +59,18 @@ app.get("/api/animals", (req, res) => {
     res.json(results);
 });
 
+// Add route for a specific animal
+app.get("/api/animals/:id", (req, res) => {
+    const result = findById(req.params.id, animals);
+    if(result) {
+        res.json(result);
+    }
+    else {
+        res.sendStatus(404);
+    }
+})
+
 // Make the server listen
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`API server on port ${PORT}`);
+app.listen(3001, () => {
+    console.log(`API server on port 3001`);
 });
